8358fc6aec305c6a9327b5460326c7e87c7e445b
Nabil Maizou <maizounabil13@gmail.com>
2024-10-01 09:34:07 +0100

    string

---
+++ Les strings/challenge2.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int main() {
+    char bomb[100];
+    int i = 0;
+    printf("entrer une chaine de caracteres : ");
+
+    scanf("%s", bomb);
+
+
+    while (bomb[i] != '\0') {
+
+
+        i++;
+        
+        
+        
+    }
+    printf("la longueur de la chaine est : %d\n", i);
+    return 0;
+    
+    
+}
